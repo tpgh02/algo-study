a5d4e743f1664ce9b9926ae1f4205cbe3df873cd
tpgh02 <tpgh02@kookmin.ac.kr>
2024-08-02 22:57:26 +0900

    week6/BOJ5052/1시간+/골드4
    주석 추가

--- week6/BOJ5052/seho.py
+++ week6/BOJ5052/seho.py
@@ -7,13 +7,14 @@ def dfs(dic, num, ite):
     global ans
     ch = num[ite]
 
-    if not (dic.get(-1) is None):
+    if not (dic.get(-1) is None): # 하나의 전화번호인지 확인
         ans = True
         return
 
     if dic.get(ch) is None:
-        dic[ch] = {}
-        if ite == ans_len-1 :
+        dic[ch] = {} # dictionary 안에 dictionary를 계속 넣어서 트리를 형성.
+        # 하나의 전화번호가 끝에 도달했을 때 dictionary 안에 {-1 : True}를 넣어줌
+        if ite == ans_len-1:
             dic[ch][-1] = True
         return
 
@@ -21,7 +22,7 @@ def dfs(dic, num, ite):
     if ite >= len(num):
         return
 
-    dfs(dic[ch], num, ite)
+    dfs(dic[ch], num, ite) # dictionary 안으로 계속 진입
 
 t = int(input())
 
