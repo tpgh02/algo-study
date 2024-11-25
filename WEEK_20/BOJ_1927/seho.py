576c976d7ab6f7b9d3750682058df1b7a39a93e8
tpgh02 <tpgh02@kookmin.ac.kr>
2024-11-25 22:27:04 +0900

    BOJ1927/5분/실버2

---
+++ WEEK_20/BOJ_1927/seho.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+hq = []
+n = int(input())
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if len(hq) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(hq))
+    else:
+        heapq.heappush(hq, x)
